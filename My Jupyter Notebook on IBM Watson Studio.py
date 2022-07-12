53bdafde2faf25884e78c917afe253c018571c25
vandrea0 <108914642+vandrea0@users.noreply.github.com>
2022-07-12 15:12:31 -0500

    Add files via upload

---
+++ My Jupyter Notebook on IBM Watson Studio.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# **Victoria Andrea Flores Sánchez**
+
+# *I am interested in data science because of the need to simply and optimize processes in my banking sector.*
+# *On the other hand, I consider myself to have an attitude of continuous learning to achieve better professional development.*
+
+# ### The next code is based on physics equation to calculate velocity, given a change in position (Δs= s2-s1) over the change in time (Δt=t2-t1) such that is represented by v=Δs/Δt.
+
+# In[12]:
+
+
+s1=100
+s2=80
+t1=25
+t2=20
+velocity=(d2-d1)/(t2-t1)
+
+print(velocity)
+
+
+# ### Using Markdown
+
+# #### Numbered List
+# 1. First item
+# 2. Second item
+# 3. Third item
+# 4. Fourth item
+# 5. Fifth item
+# 
+# 
+# &nbsp;
+# #### Table
+# | C1 | C2 | C3 | C4 |
+# | --------- | --------- | --------- | --------- |
+# | Item 1,1  | Item 1,2  | Item 1,3  | Item 1,4  | 
+# | Item 2,1  | Item 2,2  | Item 2,3  | Item 2,4  |
+# | Item 3,1  | Item 3,2  | Item 3,3  | Item 3,4  |
+# 
+# 
+# &nbsp;
+# #### Highlighted Text
+# 
+# <mark style="background-color: #FFFF00"> Hello! </mark>
